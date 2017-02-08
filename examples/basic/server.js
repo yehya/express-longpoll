88a58e9f63fb68ca99524d16713901459d582dc7
Yehya Awad <yehyaawad@hotmail.com>
2017-02-08 21:32:05 +0000

    Add longpoll by id

--- examples/basic/server.js
+++ examples/basic/server.js
@@ -3,21 +3,37 @@ var app = express();
 const longpoll = require("../../index.js")(app);
 
 app.use((req, res, next) => {
-    console.log("URL: " + req.url);
+    // console.log("URL: " + req.url);
     next();
 });
 
-longpoll.create("/poll");
 longpoll.create("/poll2");
 
-app.get('/', function(req, res) {
-    longpoll.publish("/poll", {
-        text: "Hello world LongPoll!"
-    });
+longpoll.create("/poll/:id", (req,res, next) => {
+    req.id = req.params.id;
+    next();
+});
+
+app.get('/publish', function(req, res) {
+    
     longpoll.publish("/poll2", {
         text: "Hello world LongPoll 2!"
     });
-    res.send('Hello World!')
+    
+    var userId1 = 1;
+    var userId2 = 2;
+    
+    // publish to req.id = 1
+    longpoll.publishToId("/poll/:id", userId1, {
+        text: "Published to ID 1!"
+    });
+    
+    // Publish to req.id = 2
+    longpoll.publishToId("/poll/:id", userId2, {
+        text: "Published to ID 2!"
+    });
+    
+    res.send('Published!');
 });
 
 app.listen(8080, function() {
--- index.js
+++ index.js
@@ -1,7 +1,36 @@
-var EventEmitter = require('events').EventEmitter;
+var EventEmitter2 = require('eventemitter2').EventEmitter2;
 var Promise = require("bluebird");
+var _ = require("lodash");
 
-var longpoll = function(app) {
+var longpoll = function(app, opts) {
+
+    // Default Config
+    var config = {
+        DEBUG: false,
+        eventEmitter: {
+            maxListeners: 10
+        }
+    };
+
+    // Merge options with config
+    if (opts) {
+        config = _.assign(config, opts);
+    }
+    
+    // For logging messages
+    var log = function() {
+        if (!config.DEBUG) return;
+        var args = Array.prototype.slice.call(arguments, 0);
+
+        args = _.map(args, (n) => {
+            if (typeof n !== "string") {
+                n = JSON.stringify(n, null, 4);
+            }
+            return n;
+        });
+
+        console.log(args.join(" "));
+    }
 
     var _app = app;
     if (!global.express_longpoll_emitters) {
@@ -9,38 +38,118 @@ var longpoll = function(app) {
     }
 
     var _newDispatcher = function(url, opts) {
-        var dispatcher = new EventEmitter();
+        
+        // Init EventEmitter
+        var dispatcher = new EventEmitter2({
+            wildcard: true,
+            delimiter: "."
+        });
+        
+        // Log on every event emitted
+        dispatcher.onAny((event, value) => {
+            log("Event emitted:", url + ":", event, value);
+        });
+        
         if (opts) {
             if (opts.maxListeners && opts.maxListeners > 0) {
                 dispatcher.setMaxListeners(opts.maxListeners);
             }
         }
+        
         global.express_longpoll_emitters[url] = dispatcher;
         return dispatcher;
     };
 
     var exportObj = {
-        create: function(url, opts) {
+        // Has middleware that assigns a req.id to emit events at specific users only
+        _createWithId: function(url, middleware, opts) {
+            return this._setupListener(url, "longpoll", middleware, opts);
+        },
+
+        // method that sets up ID
+        use: function(middleware) {
+            _app.use(middleware);
+        },
+
+        // Create a new longpoll
+        create: function(url, middleware, opts) {
+            if (typeof middleware === "function") {
+                return this._createWithId(url, middleware, opts);
+            }
+            else {
+                opts = middleware;
+            }
+            return this._setupListener(url, "longpoll", null, opts);
+        },
+        
+        // Publishes to everyone listening to this long poll
+        publish: function(url, data) {
             return new Promise(function(resolve, reject) {
+                if (global.express_longpoll_emitters[url]) {
+                    global.express_longpoll_emitters[url].emit('longpoll.**', data);
+                    resolve();
+                }
+                else {
+                    reject("Longpoll with the provided URL does not exist: " + url);
+                }
+            });
+        },
+        
+        // Pushes data to listeners with an extra ID
+        publishToId: function(url, id, data) {
+            return this._emit(url, "longpoll." + id, data);
+        },
+        
+        // Setup the longpoll listener in an express .get route
+        _setupListener: function(url, event, middleware, opts) {
+            if (middleware == null) {
+                middleware = (req, res, next) => next();
+            }
+            return new Promise((resolve, reject) => {
+
+                // Check if longpoll for URL already exists
                 if (global.express_longpoll_emitters[url]) {
                     return reject("URL already in use: " + url);
                 }
+
+                // Setup new dispatcher for the URL
                 var dispatcher = _newDispatcher(url, opts);
-                _app.get(url, (req, res) => {
+
+                // Setup the GET handler for a longpoll request
+                _app.get(url, middleware, (req, res) => {
+                    var eventId = "longpoll";
+
+                    // Check if there is an ID associated with the request
+                    if (req.id) {
+                        // Add the ID to the event listener
+                        eventId = eventId + "." + req.id;
+                        // Clear all previous events for the ID, we only need one
+                        log("Old Events cleared: ", url, eventId);
+                        dispatcher.removeAllListeners([eventId]);
+                    }
+
+                    // Method that Creates event listener
                     var sub = function(res) {
-                        dispatcher.once('message', function(data) {
-                            res.json(data)
+                        log("Event listener registered: ", req.url + ":", eventId);
+
+                        dispatcher.once(eventId, function(data) {
+                            log("Event listener triggered: ", req.url, eventId, "Data: ", data);
+                            res.json(data);
                         });
                     }
-                    sub(res)
+
+                    // Create it
+                    sub(res);
                 });
                 resolve();
             });
         },
-        publish: function(url, data) {
+        
+        // Emits an event to an event listener
+        _emit: function(url, event, data) {
             return new Promise(function(resolve, reject) {
                 if (global.express_longpoll_emitters[url]) {
-                    global.express_longpoll_emitters[url].emit('message', data);
+                    global.express_longpoll_emitters[url].emit(event, data);
                     resolve();
                 }
                 else {
@@ -48,6 +157,7 @@ var longpoll = function(app) {
                 }
             });
         }
+        
     }
 
     return exportObj;
